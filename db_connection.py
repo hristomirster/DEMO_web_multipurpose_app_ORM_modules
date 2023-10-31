f547904e10693456dda01946fd4730731d599058
Hristomir Vasilev <hristomirster@gmail.com>
2023-10-31 09:37:24 +0200

    Update db_connection.py

--- db_connection.py
+++ db_connection.py
@@ -1,4 +1,9 @@
 from sqlalchemy import create_engine
 
-# engine = create_engine("sqlite+pysqlite:///database.db")
-engine = create_engine('postgresql://postgres:ZWOiN2KYZj3C4Dq3n1swOosDsZWcx@18.193.123.132/pd_database')
+engine = create_engine('postgresql://postgres:ZWOiN2KYZj3C4Dq3n1swOosDsZWcx@localhost/pg_database')
+
+
+"""
+engine = create_engine("sqlite+pysqlite:///database.db")
+engine = create_engine('postgresql://postgres:password@localhost/pg_database')
+"""
