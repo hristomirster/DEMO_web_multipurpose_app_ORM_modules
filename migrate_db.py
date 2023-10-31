5e98a456c866f76f85f28461020e8c835756f3ff
Hristomir Vasilev <hristomirster@gmail.com>
2023-10-31 09:37:30 +0200

    Update migrate_db.py

--- migrate_db.py
+++ migrate_db.py
@@ -4,12 +4,13 @@ from todo import ToDo
 from shopping import Shopping
 
 engine_sqlite = create_engine('sqlite:///database.db')
-engine_postgresql = create_engine('postgresql://postgres:ZWOiN2KYZj3C4Dq3n1swOosDsZWcx@18.193.123.132/pd_database')
+# engine_postgresql = create_engine('postgresql://postgres:ZWOiN2KYZj3C4Dq3n1swOosDsZWcx@18.193.123.132/pd_database')
+engine_postgresql = create_engine('postgresql://postgres:ZWOiN2KYZj3C4Dq3n1swOosDsZWcx@localhost/pg_database')
 
 
 with engine_sqlite.begin() as conn_sqlite, engine_postgresql.begin() as conn_postgresql:
-    rows = conn_sqlite.execute(select(Shopping)).fetchall()
+    rows = conn_sqlite.execute(select(Users)).fetchall()
 
     for row in rows:
         row_dict = row._asdict()
-        conn_postgresql.execute(Shopping.__table__.insert().values(**row_dict))
+        conn_postgresql.execute(Users.__table__.insert().values(**row_dict))
