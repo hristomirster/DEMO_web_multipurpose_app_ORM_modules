1e16c629776b3140856b7b2f3513dd482161f63f
Hristomir Vasilev <hristomirster@gmail.com>
2023-10-31 18:16:41 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,16 @@ from authentication import configure_logout
 from index import configure_index_route
 from todo import configure_todo_route
 from shopping import configure_shoping_route
+from vacations import configure_vacations_route
 from users import configure_users_route
 
 app = Flask(__name__)
 
 configure_logout(app)
-
 configure_index_route(app)
-
 configure_todo_route(app)
-
 configure_shoping_route(app)
-
+configure_vacations_route(app)
 configure_users_route(app)
 
 if __name__ == '__main__':
