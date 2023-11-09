e0aa70c7fa50b8a08b286edc954002108dddd091
Hristomir Vasilev <hristomirster@gmail.com>
2023-11-09 10:06:44 +0200

    Create main_test.py

---
+++ main_test.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from authentication import configure_logout
+from index import configure_index_route
+from todo import configure_todo_route
+from shopping import configure_shoping_route
+from vacations import configure_vacations_route
+from users import configure_users_route
+
+app = Flask(__name__)
+
+configure_logout(app)
+configure_index_route(app)
+configure_todo_route(app)
+configure_shoping_route(app)
+configure_vacations_route(app)
+configure_users_route(app)
+
+if __name__ == "__main__":
+    ssl_context = ('cert.pem', 'newkey.pem')
+    app.run(ssl_context=ssl_context, host="0.0.0.0", port=5000)
